a3828ba7bfe01a2fe62fd5d63283f91cdafa25c6
Jacob Short <jrshort89@gmail.com>
2020-09-04 02:00:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  ans = {}
+  data.each { |key, val| 
+    val.each { |key2, val2| 
+      val2.each { |x| 
+      if !ans.has_key? x
+        ans[x] = {key => []}
+      elsif !ans[x][key]
+        ans[x][key] = [key2.to_s]
+      end
+        if !ans[x][key].include? key2.to_s 
+        ans[x][key] << key2.to_s
+      end
+    }
+  }
+  }
+  ans
 end
